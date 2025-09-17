ab3e999a67ad5d4deed3bb9c7f817f0026f8f2da
Alexander Raymont <Burningflame201@gmail.com>
2025-09-17 19:35:18 +0100

    implement encryption for the Vigenère cypher

--- Algorithms/VigenereCypher.py
+++ Algorithms/VigenereCypher.py
@@ -4,14 +4,32 @@
 class VigenereCypher():
     def __init__(self,key = 'password'):
         self.key = []
+        self.keyphrase=key
         self.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         for i in key:
             val = self.alphabet.index(i)
             self.key.append(val)
+        print(self.key)
 
     def encrypt(self,phrase):
-        pass
+        fin = []
+        phrase = phrase.lower()
+        loc=[]
+        count=0
+        for i in phrase:
+            if(i not in self.alphabet):
+                loc.append(' ')
+                fin.append(' ')
+            else:
+                length=(len(self.key)-1)
+                print(count % length)
+                val = (self.alphabet.index(i) + self.key[count % (len(self.key)-1)]) % 26
+                loc.append(val)
+                fin.append(self.alphabet[val])
+            count+=1
+        print(''.join(fin))
+        
 
-    def decypt(self,phrase):
+    def decrypt(self,phrase):
         pass
 
--- Testing.py
+++ Testing.py
@@ -1,5 +1,6 @@
 import Algorithms.ShiftCypher as ShiftCypher
 import Algorithms.AffineCypher as AffineCypher
+import Algorithms.VigenereCypher as VigenereCypher
 
 
 class tester():
@@ -11,8 +12,13 @@ class tester():
         encrypted = cc.encrypt(self.phrase,1)
         cc.decrypt(encrypted,1)
 
-    def test_new(self):
+    def test_affine(self):
         cc = AffineCypher.AffineCypher(4,7)
+        encrypted = cc.encrypt(self.phrase)
+        cc.decrypt(encrypted)
+
+    def test_new(self):
+        cc = VigenereCypher.VigenereCypher()
         encrypted = cc.encrypt(self.phrase)
         cc.decrypt(encrypted)
         
