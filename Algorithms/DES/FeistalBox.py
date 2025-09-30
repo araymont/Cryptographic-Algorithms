808b2dfc2004550faa6feac9c4efb155a1aaef39
Alexander Raymont <Burningflame201@gmail.com>
2025-09-30 17:04:01 +0100

    Finalise FeistalBox final permutation

--- Algorithms/DES/FeistalBox.py
+++ Algorithms/DES/FeistalBox.py
@@ -70,5 +70,12 @@ class FeistalBox():
         sBoxValue=sBoxDict[boxIn1]
         return sBoxValue[self.binaryToDecimal(boxIn2)]
 
-    def permutation(self):
-        pass
+    def permutation(self,substitutedResult):
+        indexs=[16,7,20,21,29,12,28,17,
+                1,15,23,26,5,18,31,10,
+                2,8,24,14,32,27,3,9,
+                19,13,30,6,22,11,4,25]
+        newResult = []
+        for i in indexs:
+            newResult.append(substitutedResult[i-1])
+        return newResult
